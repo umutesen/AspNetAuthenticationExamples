5d6ccd7354a67bc70a9ac80ff476a8f8f1a8aad6
Umut Esen <umutesen@me.com>
2016-12-02 14:59:37 +0000

    Add project files.

---
+++ GenericPrincipal/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Security.Principal;
+using System.Threading;
+
+namespace GenericPrincipal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SetupPrincipal();
+            UsePrincipal();
+        }
+
+        private static void SetupPrincipal()
+        {
+            // Authenticate client
+            var identity = new GenericIdentity("bob");
+
+            // fetch roles
+            var roles = new string[] { "Development", "Marketing" };
+
+            var principal = new System.Security.Principal.GenericPrincipal(identity, roles);
+
+            Thread.CurrentPrincipal = principal;
+        }
+
+        private static void UsePrincipal()
+        {
+            var p = Thread.CurrentPrincipal;
+
+            Debug.WriteLine(p.Identity.Name);
+
+            Debug.WriteLine(p.IsInRole("Development"));
+        }
+
+    }
+}
---
+++ WindowsPrincipal/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Security.Principal;
+
+namespace WindowsPrincipal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            Debug.WriteLine(identity.Name);
+
+            var account = new NTAccount(identity.Name);
+            var sid = account.Translate(typeof(SecurityIdentifier));
+            Debug.WriteLine(sid);
+
+            foreach (var group in identity.Groups.Translate(typeof(NTAccount)))
+            {
+                Debug.WriteLine(group);
+            }
+
+
+            var principal = new System.Security.Principal.WindowsPrincipal(identity);
+
+            var localAdmins = new SecurityIdentifier(WellKnownSidType.BuiltinAdministratorsSid, null);
+            var domainAdmin = new SecurityIdentifier(WellKnownSidType.AccountDomainAdminsSid, identity.User.AccountDomainSid);
+            var user = new SecurityIdentifier(WellKnownSidType.BuiltinUsersSid, null);
+
+            Debug.WriteLine(principal.IsInRole(localAdmins));
+            Debug.WriteLine(principal.IsInRole(domainAdmin));
+            Debug.WriteLine(principal.IsInRole(user));
+        }
+    }
+}
